ca4f7587fb1fb22246c7c4f53792ea82a8b623cb
Olena Tomkevych <olenatomkevych1905@gmail.com>
2024-01-22 19:06:18 -0600

    making changes - using only 2 variables

---
+++ js/homework1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let a = 23;
+let b = 29;
+console.log(a + b);
+
+
+let d = 20;
+let c = 3;
+console.log(d / c);
+
+let k = 3;
+let m = 3;
+console.log(k * m);
+
+let s = 6;
+let l = s ** 6;
+console.log(l)
