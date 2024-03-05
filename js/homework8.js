2e5584591c8084675028ef0a8fa6756929f00229
Olena Tomkevych <olenatomkevych1905@gmail.com>
2024-03-05 09:30:08 -0600

    re-work

--- js/homework8.js
+++ js/homework8.js
@@ -3,21 +3,15 @@ console.log(addThemAll(2,4)); // 6
 console.log(addThemAll(1,2,3,4)); // 10
 console.log(addThemAll(5,5,10)); // 20
 function addThemAll(...numbers) {
-let sum = 0;
-numbers.forEach(function(number)
-{
-    sum +=number;
-});
-return sum;
+return numbers.reduce((sum, number) => sum + number,0);
 }
-
 
 //TASK 2
 console.log(multiply(5)(5))     // 25
 console.log(multiply(2)(-2))    // -4
 console.log(multiply(4)(3))     // 12
 function multiply(a) {
-return function runMultiply(b){
+return function multiplier(b){
     return a * b;
 }
 }
